4714d46c188b6a5ce9d9faa174d8a3247428908f
Toby Harris <operator@tobyz.net>
2020-02-25 15:21:48 +0000

    Initial Commit

---
+++ ORBIT Camera/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ORBIT Camera
+//
+//  Created by Toby Harris on 25/02/2020.
+//  Copyright © 2020 Toby Harris. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
