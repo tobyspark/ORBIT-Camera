935e2cd96c2c06013cdc1f7366b84755510de50b
Toby Harris <operator@tobyz.net>
2020-03-20 10:18:37 +0000

    Refactor with NewThingCell

---
+++ ORBIT Camera/NewThingCell.swift
@@ -0,0 +1,13 @@
+//
+//  ThingCell.swift
+//  ORBIT Camera
+//
+//  Created by Toby Harris on 20/03/2020.
+//  Copyright © 2020 Toby Harris. All rights reserved.
+//
+
+import UIKit
+
+class NewThingCell: UITableViewCell {
+    @IBOutlet weak var labelField: UITextField!
+}
