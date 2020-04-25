302e116e49f626ca9cfc3e3f903cbe47966c6a60
Toby Harris <operator@tobyz.net>
2020-04-25 17:49:10 +0100

    Remove unlock code UI from info scene

--- ORBIT Camera/InfoViewController.swift
+++ ORBIT Camera/InfoViewController.swift
@@ -17,30 +17,21 @@ class InfoViewController: UIViewController {
     
     @IBOutlet weak var scrollView: UIScrollView!
     
-    @IBOutlet weak var unlockCode: UITextField!
-    @IBOutlet weak var unlockCodeStatus: UILabel!
-    
-    @IBOutlet weak var introWebView: WKWebView!
-    @IBOutlet weak var introWebViewHeightContstraint: NSLayoutConstraint!
-    
-    @IBOutlet weak var tutorialWebView: WKWebView!
-    @IBOutlet weak var tutorialWebViewHeightContstraint: NSLayoutConstraint!
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var webViewHeightContstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        introWebView.navigationDelegate = self
-        let introHTML = MarkdownParser.html(markdownResource: "Introduction")
-        introWebView.loadHTMLString(introHTML, baseURL: nil)
-        
-        tutorialWebView.navigationDelegate = self
-        let tutorialHTML = MarkdownParser.html(markdownResource: "TutorialScript")
-        tutorialWebView.loadHTMLString(tutorialHTML, baseURL: nil)
-        
-        if let credential = try! Participant.appParticipant().authCredential { // FIXME: try!
-            unlockCode.text = credential
-            checkCredential(credential)
-        }
+        webView.navigationDelegate = self
+        let introHTML = MarkdownParser.html(markdownResource: "TutorialScript")
+        webView.loadHTMLString(introHTML, baseURL: nil)
+
+// TODO: Move to first-run
+//        if let credential = try! Participant.appParticipant().authCredential { // FIXME: try!
+//            unlockCode.text = credential
+//            checkCredential(credential)
+//        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -70,82 +61,83 @@ class InfoViewController: UIViewController {
             scrollView!
         ]
     }
-    
-    @IBAction func unlockCodeEditingDidEnd(_ sender: Any) {
-        if let credential = unlockCode.text {
-            checkCredential(credential)
-        }
-    }
-    
-    enum CredentialError: Error {
-        case transportError
-        case responseError
-        case unexpectedResponse
-        case credentialRejected
-    }
-    
-    func checkCredential(_ credential: String) {
-        // Test the auth credential by hitting an API endpoint
-        let url = URL(string: Settings.endpointThing)!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue(credential, forHTTPHeaderField: "Authorization")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let task = URLSession.shared.dataTask(with: request) { (_, response, error) in
-            let result: Result<String, CredentialError>
-            if error != nil {
-                result = .failure(.transportError)
-            } else {
-                if let httpResponse = response as? HTTPURLResponse {
-                    switch httpResponse.statusCode {
-                    case 200...299:
-                        result = .success(credential) // TODO: also return the ORBIT participant ID, having hit a participant end-point and decoding the json
-                    case 403:
-                        result = .failure(.credentialRejected)
-                    default:
-                        result = .failure(.unexpectedResponse)
-                    }
-                } else {
-                    result = .failure(.responseError)
-                }
-            }
-            if case .failure(let error) = result {
-                os_log("%{public}@", error.localizedDescription)
-            }
-            DispatchQueue.main.async {
-                self.handleCredentialResult(result)
-            }
-        }
-        task.resume()
-        unlockCodeStatus.textColor = .label
-        unlockCodeStatus.text = "Verifying credential..."
-    }
-    
-    func handleCredentialResult(_ result: Result<String, CredentialError>) {
-        switch result {
-        case .success(let credential):
-            // UI
-            unlockCodeStatus.textColor = .systemGreen
-            unlockCodeStatus.text = "Code accepted"
-            // Save validated credential
-            var participant = try! Participant.appParticipant() // FIXME: try!
-            participant.authCredential = credential
-            try! dbQueue.write { db in try participant.save(db) } // FIXME: try!
-        case .failure(let error):
-            unlockCodeStatus.textColor = .systemRed
-            switch error {
-            case .transportError:
-                unlockCodeStatus.text = "Could not verify: transport error"
-            case .responseError:
-                unlockCodeStatus.text = "Could not verify: error in server response"
-            case .unexpectedResponse:
-                unlockCodeStatus.text = "Could not verify: unexpected server response"
-            case .credentialRejected:
-                unlockCodeStatus.text = "Code rejected"
-            }
-        }
-    }
+
+// TODO: Move to first-run
+//    @IBAction func unlockCodeEditingDidEnd(_ sender: Any) {
+//        if let credential = unlockCode.text {
+//            checkCredential(credential)
+//        }
+//    }
+//
+//    enum CredentialError: Error {
+//        case transportError
+//        case responseError
+//        case unexpectedResponse
+//        case credentialRejected
+//    }
+//
+//    func checkCredential(_ credential: String) {
+//        // Test the auth credential by hitting an API endpoint
+//        let url = URL(string: Settings.endpointThing)!
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "GET"
+//        request.setValue(credential, forHTTPHeaderField: "Authorization")
+//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//
+//        let task = URLSession.shared.dataTask(with: request) { (_, response, error) in
+//            let result: Result<String, CredentialError>
+//            if error != nil {
+//                result = .failure(.transportError)
+//            } else {
+//                if let httpResponse = response as? HTTPURLResponse {
+//                    switch httpResponse.statusCode {
+//                    case 200...299:
+//                        result = .success(credential) // TODO: also return the ORBIT participant ID, having hit a participant end-point and decoding the json
+//                    case 403:
+//                        result = .failure(.credentialRejected)
+//                    default:
+//                        result = .failure(.unexpectedResponse)
+//                    }
+//                } else {
+//                    result = .failure(.responseError)
+//                }
+//            }
+//            if case .failure(let error) = result {
+//                os_log("%{public}@", error.localizedDescription)
+//            }
+//            DispatchQueue.main.async {
+//                self.handleCredentialResult(result)
+//            }
+//        }
+//        task.resume()
+//        unlockCodeStatus.textColor = .label
+//        unlockCodeStatus.text = "Verifying credential..."
+//    }
+//
+//    func handleCredentialResult(_ result: Result<String, CredentialError>) {
+//        switch result {
+//        case .success(let credential):
+//            // UI
+//            unlockCodeStatus.textColor = .systemGreen
+//            unlockCodeStatus.text = "Code accepted"
+//            // Save validated credential
+//            var participant = try! Participant.appParticipant() // FIXME: try!
+//            participant.authCredential = credential
+//            try! dbQueue.write { db in try participant.save(db) } // FIXME: try!
+//        case .failure(let error):
+//            unlockCodeStatus.textColor = .systemRed
+//            switch error {
+//            case .transportError:
+//                unlockCodeStatus.text = "Could not verify: transport error"
+//            case .responseError:
+//                unlockCodeStatus.text = "Could not verify: error in server response"
+//            case .unexpectedResponse:
+//                unlockCodeStatus.text = "Could not verify: unexpected server response"
+//            case .credentialRejected:
+//                unlockCodeStatus.text = "Code rejected"
+//            }
+//        }
+//    }
 }
 
 extension InfoViewController: WKNavigationDelegate {
@@ -153,12 +145,7 @@ extension InfoViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         webView.evaluateJavaScript("document.documentElement.scrollHeight") { [weak self] (height, error) in
             guard let self = self else { return }
-            if webView === self.introWebView {
-                self.introWebViewHeightContstraint.constant = height as! CGFloat
-            }
-            if webView === self.tutorialWebView {
-                self.tutorialWebViewHeightContstraint.constant = height as! CGFloat
-            }
+            self.webViewHeightContstraint.constant = height as! CGFloat
         }
     }
     
