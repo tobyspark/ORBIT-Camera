5efb4e6faec46f8d88a0c035093d5647f6c9b724
Toby Harris <operator@tobyz.net>
2020-04-30 18:15:55 +0100

    Settings+secrets – how to ignore further changes

--- ORBIT Camera/Settings+secrets.swift
+++ ORBIT Camera/Settings+secrets.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2020 Toby Harris. All rights reserved.
 //
 
+/// Further changes to this file should not be tracked in Git.
+/// The file is included in Git so the project compiles and future users have an idea what's going on.
+///
+/// Relevant Git arcana –
+/// `git update-index --assume-unchanged 'ORBIT Camera/Settings+secrets.swift'`
+///
+
 import Foundation
 
 extension Settings {
