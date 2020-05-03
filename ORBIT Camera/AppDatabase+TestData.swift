4052693ca1aa55217840c8d630d614e11c3346d4
Toby Harris <operator@tobyz.net>
2020-05-03 01:58:26 +0100

    Test data has randomised video properties

--- ORBIT Camera/AppDatabase+TestData.swift
+++ ORBIT Camera/AppDatabase+TestData.swift
@@ -57,6 +57,8 @@ extension AppDatabase {
                         assertionFailure("When loading test data could not locate video file")
                     }
                     var video = Video(of: thing, url: videoURL, kind: videoKind)!
+                    video.recorded = Date(timeIntervalSinceNow: -Double.random(in: 0...7*24*60*60))
+                    video.orbitID = 1
                     try dbQueue.write { db in try video.save(db) }
                 }
             }
